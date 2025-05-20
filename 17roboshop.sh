e246f5609c2b01c2644e509201b79745d49166e6
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-20 16:25:37 +0530

    robofile changes

--- 17roboshop.sh
+++ 17roboshop.sh
@@ -8,7 +8,7 @@ DOMAIN_NAME="daws84s.space"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCES_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --security-group-ids sg-01ade26956bf74218 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instances}]" --query "Instances[0].InstanceID" --output text)
+    INSTANCES_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --security-group-ids sg-01ade26956bf74218 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceID" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
