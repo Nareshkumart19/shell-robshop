8a4ba08efb8a3a10fb1c5bb9212a19f3f287225b
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-21 15:11:20 +0530

    robofile changes

---
+++ 18 roboshop with rote 53.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SGI_ID="sg-01ade26956bf74218"
+INSTANCES=("mongodb" "cataloge" "fronend")  
+ZONE_ID="Z10073371KESZAOI9YC5L"
+DOMAIN_NAME="daws84s.space"
+
+for instance in ${INSTANCES[@]}
+do
+    INSTANCES_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --security-group-ids sg-01ade26956bf74218 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceID" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+       
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+    echo "$instance  IP address: $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Creating are updateing a record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "upset"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$instance'.'$DOMAIN_NAME"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "$IP"
+        }]
+      }
+    }]
+  }
+done 
+
+
