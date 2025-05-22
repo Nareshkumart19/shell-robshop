2d1f8c66961c21c1d23746dc21ff236941c60889
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-22 17:35:25 +0530

    robofile changes

--- frontend.sh
+++ frontend.sh
@@ -34,10 +34,10 @@ VALIDATE(){
 }
 
 
-dnf module disable nginx -y &>>$LOG_FILE
+dnf module disable nginx -y
 VALIDATE $? "Disabling Default Nginx"
 
-dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y
 VALIDATE $? "Enabling Nginx:1.24"
 
 dnf install nginx -y &>>$LOG_FILE
@@ -53,6 +53,7 @@ VALIDATE $? "Removing default content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
+
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "unzipping frontend"
@@ -62,6 +63,7 @@ VALIDATE $? "Remove default nginx conf"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
+
 
 systemctl restart nginx 
 VALIDATE $? "Restarting nginx"
