3a9553e23c748a987fbe392e1e1e24867ae68a34
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-23 18:33:09 +0530

    robofile changes

--- 18roboshopwithrote53 copy.sh
+++ 18roboshopwithrote53 copy.sh
@@ -11,16 +11,16 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01ade26956bf74218 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
-         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
      
     else
-         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
     echo "$instance  IP address: $IP"
 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
+     --hosted-zone-id $ZONE_ID \
+     --change-batch '
       {  "Comment": "Creating and update a record set for cognito endpoint"
       ,"Changes": [{
           "Action"              : "UPSERT"
