97b44494ef4d0b8c2cea7fed13850d139eef3e4e
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-24 17:12:26 +0530

    code change

--- redis.sh
+++ redis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
+start_time=$(date +%s) 
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -33,25 +33,24 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disabling Default Redis version"
+dnf module disable redis -y &>>$LOG_FILE 
+VALIDATE $? "disable redis modules"
 
-dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Redis:7"
+dnf module enable redis:7 -y &>>$LOG_FILE 
+VALIDATE $? "enable redis modules"
 
-dnf install redis -y &>>$LOG_FILE
-VALIDATE $? "Installing Redis"
+dnf install redis -y  &>>$LOG_FILE 
+VALIDATE " installing redis pakages"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e  's/127.0.0.1/0.0.0.0/g'  -e '/protected-mode/ c protected-mode no/' /etc/redis/redis.conf &>>$LOG_FILE
 VALIDATE $? "Edited redis.conf to accept remote connections"
 
-systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "Enabling Redis"
-
+systemctl enable redis  &>>$LOG_FILE
 systemctl start redis  &>>$LOG_FILE
-VALIDATE $? "Started Redis"
+VALIDATE   $? "enabling and staring service"
+ 
+end_time=$(date +%s)
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+total_time=$((end_time - start_time))
+echo "Total execution time:   Y $total_time seconds" $N | tee -a &>>$LOG_FILE 
 
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
